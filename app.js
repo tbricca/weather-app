67e6ca3c1e458690d1c5d1e0fef90e8c0da11a0e
tbricca <tbricca@gmail.com>
2019-02-10 10:25:20 -0800

    utilized fetch to call api

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ window.addEventListener('load', ()=> {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const proxy ='https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/f042b5305e12c72ca75fb5bc2fa0cdd5/${lat},${long}`;
+            
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data =>{
+                    console.log(data);
+            });
         });
-    }else{
-        h1.textContent = "You need to enable your location to make this work :)"
-    }
+    } 
 });
+
+    
+const Http = new XMLHttpRequest();
+const url = 'https://api.darksky.net/forecast/f042b5305e12c72ca75fb5bc2fa0cdd5/37.8267,-122.4233'
